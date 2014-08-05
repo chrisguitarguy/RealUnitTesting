61f76dcaa58d0879e40b533aac6478ea474febed
Christopher Davis <cdavis9999@gmail.com>
2014-08-04 21:18:56 -0400

    Get demeanor set up

---
+++ test/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Real Unit Testing
+ *
+ * Code for a talk given at Northeast PHP 2014: http://www.northeastphp.org/
+ *
+ * @copyright   2014 Christopher Davis
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->addPsr4('Chrisguitarguy\\RealUnitTesting\\', __DIR__);
