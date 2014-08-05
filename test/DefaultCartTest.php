a3d4d9a43be1688c10397538408d09c893150776
Christopher Davis <cdavis9999@gmail.com>
2014-08-04 21:23:31 -0400

    Start with a Test Stub

---
+++ test/DefaultCartTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Real Unit Testing
+ *
+ * Code for a talk given at Northeast PHP 2014: http://www.northeastphp.org/
+ *
+ * @copyright   2014 Christopher Davis
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Chrisguitarguy\RealUnitTesting;
+
+class DefaultCartTest
+{
+    use \Counterpart\Assert;
+
+    // we will change this name
+    public function testTotal()
+    {
+        
+    }
+}
